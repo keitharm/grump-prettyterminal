028e8fc26caeb5715b9d3a5475ac39847d8e9eec
Keith Armstrong <keith1015@gmail.com>
2015-10-05 17:22:10 -0700

    Load the new terminal settings without having to open a new tab

--- prettyterminal.sh
+++ prettyterminal.sh
@@ -4,3 +4,4 @@ echo 'export PS1="\[\033[36m\]\u\[\033[m\]@\[\033[32m\]\h:\[\033[33;1m\]\w\[\033
 echo 'export CLICOLOR=1' >> ~/.bash_profile
 echo 'export LSCOLORS=ExFxBxDxCxegedabagacad' >> ~/.bash_profile
 echo "alias ls='ls -GFh'" >> ~/.bash_profile
+source ~/.bash_profile
